cb8c811f2aa04f94f78a3f60c1baa6a67a67f8bd
iarla-crewe <iarla.crewe@gmail.com>
2024-11-26 15:57:21 +0000

    Add drift data calculations

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,4 @@
 export const DRIFT_MARKET_INDEX_USDC = 0;
 export const DRIFT_MARKET_INDEX_SOL = 1;
 export const MICRO_CENTS_PER_USDC = 1000000;
+export const QUARTZ_HEALTH_BUFFER_PERCENTAGE = 10;
--- src/controllers/drift.controller.ts
+++ src/controllers/drift.controller.ts
@@ -1,7 +1,10 @@
-import { DriftClient, Wallet } from "@drift-labs/sdk";
+import { calculateBorrowRate, calculateDepositRate, DriftClient, Wallet } from "@drift-labs/sdk";
 import { Connection, Keypair } from "@solana/web3.js";
 import config from "../config/config";
 import { NextFunction, Request, Response } from "express";
+import { bnToDecimal, getQuartzHealth, retryRPCWithBackoff } from "../utils/helpers";
+import { DriftUser } from "../models/driftUser";
+import { PublicKey } from "@solana/web3.js";
 
 export class DriftController {
     private connection: Connection;
@@ -23,27 +26,88 @@ export class DriftController {
         this.initPromise = this.driftClient.subscribe();
     }
 
+    private async getUser(address: string) {
+        const driftUser = new DriftUser(new PublicKey(address), this.connection, this.driftClient!);
+        await retryRPCWithBackoff(
+            async () => driftUser.initialize(),
+            3,
+            500
+        );
+        return driftUser;
+    }
+
     public getRate = async (req: Request, res: Response, next: NextFunction) => {
         await this.initPromise;
 
+        const marketIndicesParam = req.query.marketIndices as string;
+        const marketIndices = marketIndicesParam.split(',').map(Number).filter(n => !isNaN(n));
 
+        const promises = marketIndices.map(async (index) => {
+            const spotMarket = await this.driftClient.getSpotMarketAccount(index);
+            if (!spotMarket) return next(new Error(`Could not find spot market for index ${index}`));
+        
+            const depositRateBN = calculateDepositRate(spotMarket);
+            const borrowRateBN = calculateBorrowRate(spotMarket);
+        
+            return {
+                depositRate: bnToDecimal(depositRateBN, 6),
+                borrowRate: bnToDecimal(borrowRateBN, 6)
+            };
+        });
+
+        const rates = await Promise.all(promises);
+        res.status(200).json(rates);
     }
 
     public getBalance = async (req: Request, res: Response, next: NextFunction) => {
         await this.initPromise;
 
-        
+        const address = req.query.address as string;
+        const marketIndicesParam = req.query.marketIndices as string;
+        const marketIndices = marketIndicesParam.split(',').map(Number).filter(n => !isNaN(n));
+
+        try {
+            const driftUser = await this.getUser(address);
+            const balances = await Promise.all(marketIndices.map(async (index) => {
+                return driftUser.getTokenAmount(index);
+            }));
+
+            res.status(200).json(balances);
+        } catch (error) {
+            next(error);
+        }
     }
 
     public getWithdrawLimit = async (req: Request, res: Response, next: NextFunction) => {
         await this.initPromise;
 
-        
+        const address = req.query.address as string;
+        const marketIndicesParam = req.query.marketIndices as string;
+        const marketIndices = marketIndicesParam.split(',').map(Number).filter(n => !isNaN(n));
+
+        try {
+            const driftUser = await this.getUser(address);
+            const withdrawLimits = await Promise.all(marketIndices.map(async (index) => {
+                return driftUser.getWithdrawalLimit(index, false).toNumber();
+            }));
+            res.status(200).json(withdrawLimits);
+        } catch (error) {
+            next(error);
+        }
     }
 
     public getHealth = async (req: Request, res: Response, next: NextFunction) => {
         await this.initPromise;
 
-        
+        const address = req.query.address as string;
+
+        try {
+            const driftUser = await this.getUser(address);
+            const driftHealth = driftUser.getHealth();
+            const quartzHealth = getQuartzHealth(driftHealth);
+            res.status(200).json(quartzHealth);
+        } catch (error) {
+            next(error);
+        }
     }
 }
