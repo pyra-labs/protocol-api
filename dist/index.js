f498e6a70725c88f3a37f52dbd7f5bb1e84eca86
Diego Garcia <83479019+Nelo0@users.noreply.github.com>
2025-02-26 17:37:26 -0700

    Delete dist folder

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import { App } from "./app.js";
-import { DataRoute } from "./routes/data.route.js";
-import { UserRoute } from "./routes/user.route.js";
-const app = new App([
-    new DataRoute(),
-    new UserRoute(),
-]);
-app.listen();
-//# sourceMappingURL=index.js.map
