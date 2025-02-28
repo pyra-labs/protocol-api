d80fba89aaed56ce2a3c831ec46519a8a58509a2
Diego Garcia <83479019+Nelo0@users.noreply.github.com>
2025-02-28 06:57:13 -0700

    Add tx controller and tx route

--- src/routes/program.route.ts
+++ src/routes/program.route.ts
@@ -1,6 +1,7 @@
 import { CompositeRoute } from "./compositeRoute.js";
 import { BuildTxRoute } from "./program/buildTx.route.js";
 import { ProgramDataRoute } from "./program/programData.route.js";
+import { TxRoute } from "./program/tx.route.js";
 
 export class ProgramRoute extends CompositeRoute {
     constructor() {
@@ -9,6 +10,7 @@ export class ProgramRoute extends CompositeRoute {
             [
                 new BuildTxRoute(),
                 new ProgramDataRoute(),
+                new TxRoute()
             ]
         );
     }
