c915a8930452a264b2f20995ce3c2c3b187d3ed0
Diego Garcia <83479019+Nelo0@users.noreply.github.com>
2025-02-28 07:03:08 -0700

    Add ProgramRoute to global set up

--- globalSetup.ts
+++ globalSetup.ts
@@ -1,11 +1,13 @@
 import { App } from "./src/app.ts";
 import { DataRoute } from "./src/routes/data.route";
 import { UserRoute } from "./src/routes/user.route";
+import { ProgramRoute } from "./src/routes/program.route";
 
 export default async () => {
       const app = new App([
             new DataRoute(),
             new UserRoute(),
+            new ProgramRoute()
       ]);
       await app.listen();
 };
